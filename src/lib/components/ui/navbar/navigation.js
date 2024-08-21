d132e01ab856ab27c6f4c45e45da395347ac768d
Steve Torrefranca <s.torrefranca@gmail.com>
2024-08-22 02:28:09 +0800

    Navigation

--- src/lib/components/ui/navbar/navigation.js
+++ src/lib/components/ui/navbar/navigation.js
@@ -1,11 +1,12 @@
 const navItems = [
+	{ name: 'Home', href: '/' },
 	{ name: 'Products', href: '/products' },
 	{ name: 'Investor', href: '/investor' },
 	{ name: 'Anchor', href: '/anchor' },
 	{ name: 'Referral', href: '/referral' },
 	{ name: 'About Us', href: '/about-us' },
 	{ name: 'FAQ', href: '/faq' },
-	{ name: 'Contact Us', href: '/contact' }
+	{ name: 'Contact Us', href: '/contact-us' }
 ];
 
 export { navItems };
