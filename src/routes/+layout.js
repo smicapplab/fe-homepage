3ae6a22f8b6dffcb2a5cf77dfc10812bdb44baa1
Steve Torrefranca <s.torrefranca@gmail.com>
2024-08-24 15:55:08 +0800

    Animated transitions

---
+++ src/routes/+layout.js
@@ -0,0 +1,6 @@
+export const load = ({ url }) => {
+ return {
+    url: url.pathname,
+    
+ }   
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,20 @@
 <script>
 	import { Footer } from '$lib/components/ui/footer';
 	import { Navbar } from '$lib/components/ui/navbar';
+	import { fly } from 'svelte/transition';
 	import '../app.css';
+
+	export let data;
+	console.log(data.url);
 </script>
 
 <div>
 	<Navbar />
-	<slot></slot>
+	{#key data.url}
+		<!-- This is for page navigation animation -->
+		<div in:fly={{ duration: 500, x: -200, delay: 200 }} out:fly={{ x: 300, duration: 200 }}>
+			<slot></slot>
+		</div>
+	{/key}
 	<Footer />
 </div>
